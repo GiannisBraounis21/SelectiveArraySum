ed339b9f204a29aa0966cfab3b40982a41d2e249
dit18131 <dit18131@office365.uop.gr>
2020-05-14 13:10:18 +0300

    Step 3 Implement main body

--- SelectiveArraySum/Source.asm
+++ SelectiveArraySum/Source.asm
@@ -1,5 +1,5 @@
 TItle Selective Array Summation
-INCLUDE Irvine32.lib
+INCLUDE Irvine32.inc
 
 .data
 array SDWORD 10,60,20,33,72,89,45,65,72,18
@@ -12,6 +12,13 @@ SelectiveSummation PROC
 	ret
 SelectiveSummation ENDP
 main PROC
+	push OFFSET sum
+	push sample
+	push OFFSET array
+	call SelectiveSummation ;int SelectiveSummation(*array,int sample ,int *sum)
+	mov edx,OFFSET message
+	call WriteString
+	call WriteInt
 	exit
 main ENDP
 END main
